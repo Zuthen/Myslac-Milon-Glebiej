e1614024c49c940715cf61fe5be9aa58b9774ebd
Zuthen <winstonivo@gmail.com>
2025-09-18 15:24:06 +0200

    QuestionAndAnswers rewritten for json model

--- myslac-milion-glebiej/src/components/QuestionAndAnswers/QuestionAndAnswers.tsx
+++ myslac-milion-glebiej/src/components/QuestionAndAnswers/QuestionAndAnswers.tsx
@@ -1,25 +1,41 @@
 import { Question } from "../Question/Question"
 import { Answer } from "../Answer/Answer"
 
-type Props = {
-    question: string,
-    answerA: string,
-    answerB: string,
-    answerC: string,
-    answerD: string,
+type Answer = {
+    answer: string,
+    correct: boolean
 }
-export const QuestionAndAnswers = ({ question, answerA, answerB, answerC, answerD }: Props) => {
+type Answers = {
+    A: Answer,
+    B: Answer,
+    C: Answer,
+    D: Answer
+}
+
+type Question = {
+    id: string,
+    question: string,
+    level: number[],
+    answers: Answers,
+    why: string
+}
+
+type Props = {
+    question: Question
+}
+
+export const QuestionAndAnswers = ({ question }: Props) => {
     return <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
         <div style={{ flex: 1 }}>
-            <Question text={question} />
+            <Question text={question.question} />
         </div>
         <div style={{ flex: 1, flexDirection: "row", display: "flex", margin: "5px" }}>
-            <div style={{ flex: 1, margin: "8px" }}> <Answer text={answerA} answerId="A" /></div>
-            <div style={{ flex: 1, margin: "8px" }}> <Answer text={answerB} answerId="B" /></div>
+            <div style={{ flex: 1, margin: "8px" }}> <Answer text={question.answers.A.answer} answerId="A" /></div>
+            <div style={{ flex: 1, margin: "8px" }}> <Answer text={question.answers.B.answer} answerId="B" /></div>
         </div>
         <div style={{ flex: 1, flexDirection: "row", display: "flex" }}>
-            <div style={{ flex: 1, margin: "8px" }}> <Answer text={answerC} answerId="C" /></div>
-            <div style={{ flex: 1, margin: "8px" }}> <Answer text={answerD} answerId="D" /></div>
+            <div style={{ flex: 1, margin: "8px" }}> <Answer text={question.answers.C.answer} answerId="C" /></div>
+            <div style={{ flex: 1, margin: "8px" }}> <Answer text={question.answers.D.answer} answerId="D" /></div>
         </div>
     </div>
 }
